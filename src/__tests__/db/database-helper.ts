f63c7ed9b9a7fd10df54f90b6c6870cf8a34669b
Nazanin Anabestani <n.annabestani@tagrow.net>
2022-02-20 21:32:12 +0330

    use embeded mongo db in tests

--- src/__tests__/db/database-helper.ts
+++ src/__tests__/db/database-helper.ts
@@ -1,8 +1,21 @@
 import {CacheRepository} from '../../repositories';
-import {config} from './mongodb.config';
 import {MongodbDataSource} from '../../datasources';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
 export async function givenEmptyDatabase() {
+  const mongod = await MongoMemoryServer.create();
+  const uri = mongod.getUri();
+
+  const config = {
+    name: 'mongodb',
+    connector: 'mongodb',
+    uri : uri,
+    user: '',
+    password: '',
+    database: 'fashion_cloud',
+    useNewUrlParser: true
+  };
+
   const cacheRepository = new CacheRepository(new MongodbDataSource(config));
   await cacheRepository.deleteAll();
 }
